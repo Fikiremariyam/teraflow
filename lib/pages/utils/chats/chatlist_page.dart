960d8c0884a94f36708a5e15e191ccfcec07d934
kalM121 <kalkidanmelaku97@gamil.com>
2025-01-23 17:42:08 +0300

    resolved conflicts

--- lib/pages/utils/chats/chatlist_page.dart
+++ lib/pages/utils/chats/chatlist_page.dart
@@ -6,58 +6,56 @@ import 'chatpage_main.dart';
 class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-return   Scaffold(
-      appBar: AppBar(title: Text("your Chats"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("your Chats"),
+      ),
       body: FutureBuilder(
-        future:  FirebaseFirestore.instance.collection('chats').where('users',arrayContains: FirebaseAuth.instance.currentUser!.email).get() , 
-        builder: (context,snapshot){
-          if (snapshot.hasData){
-            if (snapshot.data!.docs.isEmpty == true){
-              return Text("no chat yets");
+          future: FirebaseFirestore.instance
+              .collection('chats')
+              .where('users',
+                  arrayContains: FirebaseAuth.instance.currentUser!.email)
+              .get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data!.docs.isEmpty == true) {
+                return Text("no chat yets");
+              }
+              return ListView.builder(
+                  itemCount: snapshot.data?.docs.length ?? 0,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot doc = snapshot.data!.docs[index];
+                    var senderemail = FirebaseAuth.instance.currentUser!.email;
+                    var reciveremail;
+
+                    for (var userDetails in doc['users']) {
+                      if (userDetails.toString() != senderemail.toString()) {
+                        reciveremail = userDetails;
+                      }
+                    }
+                    ;
+
+                    return ListTile(
+                      title: Text(reciveremail),
+                      subtitle: Text(doc['recent_text']),
+                      onTap: () {
+                        print("sent snap shots");
+                        print(doc.reference.collection('messages').doc().get());
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ChatpageMain(
+                                  doc: doc,
+                                  recieverEmail: reciveremail,
+                                )));
+                      },
+                      trailing: Icon(
+                        Icons.arrow_forward,
+                      ),
+                    );
+                  });
+            } else {
+              return CircularProgressIndicator();
             }
-            return ListView.builder(
-              itemCount: snapshot.data?.docs.length ?? 0 ,
-              itemBuilder:(context, index){
-                DocumentSnapshot doc = snapshot.data!.docs[index];
-                 var  senderemail = FirebaseAuth.instance.currentUser!.email;
-                 var  reciveremail;
-
-                 for (var userDetails in doc['users']) {
-                      if (userDetails.toString()  != senderemail.toString()) {
-                              reciveremail = userDetails;
-                            }
-                          };
-                    
-                return ListTile(
-                  
-
-                  title : Text( reciveremail )
-                  ,
-                  subtitle: Text(doc['recent_text']),
-                  onTap: (){
-                    print("sent snap shots");
-                    print(doc.reference.collection('messages').doc().get());
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ChatpageMain(doc: doc,recieverEmail: reciveremail,)));
-
-                  },
-                  trailing: Icon(
-
-                    Icons.arrow_forward,
-                    
-                  ),
-                  );
-                
-              });
-          }else{
-           return  CircularProgressIndicator();
-          }
-          
-          
-         
-        }
-        ),
-
+          }),
     );
   }
-  }
-
+}
