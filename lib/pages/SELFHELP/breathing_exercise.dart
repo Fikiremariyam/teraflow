9c20dacb66235d89894e452b0a170c1bcdd6896f
Fikiremariyam <dagi123baba@gmail.com>
2025-02-10 16:28:05 +0300

    fix video page

--- lib/pages/SELFHELP/breathing_exercise.dart
+++ lib/pages/SELFHELP/breathing_exercise.dart
@@ -1,5 +1,4 @@
 import 'package:flick_video_player/flick_video_player.dart';
-import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:get/get.dart';
@@ -60,7 +59,6 @@ class _BreathingExerciseDetailPageState
 
       if (_videoData != null && _videoData!.isNotEmpty) {
        
-       
         _initializeVideo();
       } else {
         throw Exception("No video data found.");
@@ -77,42 +75,19 @@ class _BreathingExerciseDetailPageState
     }
   }
 
-  void _initializeVideo() {
-    if (_videoData == null || _videoData!.isEmpty) return;
-
-    _videoPlayerController = VideoPlayerController.asset(
-        _videoData![_currentVideoIndex]['videoUrl']);
-    _videoPlayerController.initialize().then((_) {
-      setState(() {});
-    }).catchError((error) {
-      print("Error initializing video: $error");
-      Get.snackbar(
-        'Error',
-        'Failed to load video',
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.red,
-        colorText: Colors.white,
-      );
-    });
-  }
-
-  void _changeVideo(int index) {
-    if (_videoData == null || index < 0 || index >= _videoData!.length) return;
-
-    _currentVideoIndex = index;
-    _videoPlayerController.dispose();
-    _initializeVideo();
-    Get.snackbar(
-      'Video Changed',
-      'Now playing: ${_videoData![_currentVideoIndex]['title']}',
-      snackPosition: SnackPosition.BOTTOM,
-    );
+  @override
+  void initState() {
+    super.initState();
+    //_loadVideoData();
+    flickmanager =FlickManager( 
+      videoPlayerController:
+        VideoPlayerController.asset("assets/video-1.mp4"));
+        
   }
 /*
 
   @override
   void dispose() {
-    if (_videoPlayerController != null) {
     if (_videoPlayerController != null) {
     _videoPlayerController.dispose();
   }
@@ -150,8 +125,6 @@ class _BreathingExerciseDetailPageState
                      child: _videoPlayerController.value.isInitialized
                         ? CustomVideoPlayer(
                             controller: _videoPlayerController,
-                            onNextVideo: (){},
-                            onPreviousVideo: (){}
                             onNextVideo: (){},
                             onPreviousVideo: (){}
                           )
